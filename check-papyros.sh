6cc767896ad09260d9207e8c86e6e4ea2ce801cc
hselin <hselin.chen@gmail.com>
2019-05-22 17:52:15 -0700

    Changes - Modified common/config to use papyros mount options in test mount - Modified common/dmerror, common/dmflakey and common/dmthin to use papyros - Modified common/papyros to use papyros mount options - Modified common/papyros to use default_permissions in _papyros_mount - Modified common/papyros to do sync before unmount - this prevents device busy errors on subsequent mount - Modified common/rc to pass mount options - Modified tests/generic/029 to use kernelcache,0readbuffer - Modified tests/generic/030 to use kernelcache - Modified tests/generic/069 to use kernelcache,writelog - Modified tests/generic/098 to use 0readbuffer - Modified tests/generic/101 to use 0readbuffer - Modified tests/generic/129 to use kernelcache - Modified tests/generic/130 to use 0readbuffer - Modified tests/generic/141 to use kernelcache - Modified tests/generic/322 to use 0readbuffer - Modified tests/generic/325 to use kernelcache - Modified tests/generic/346 to use kernelcache - Modified tests/generic/347 to mkfs papyros with DMTHIN_VOL_DEV in _setup_thin - Modified tests/generic/393 to use 0readbuffer - Modified tests/generic/409 to handle papyros correctly - Modified tests/generic/410 to handle papyros correctly - Modified tests/generic/411 to handle papyros correctly - Modified tests/generic/412 to use 0readbuffer - Modified tests/generic/456 to use kernelcache

--- check-papyros.sh
+++ check-papyros.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-
 #set -x
 
 if [[ $EUID -ne 0 ]]; then
@@ -28,7 +27,12 @@ export SCRATCH_SPARE_DEV=/dev/loop3
 export SCRATCH_MNT=/mnt/xfstests/scratch_dir
 export SCRATCH_MD_DIR=$MD_VOL$SCRATCH_DEV
 
+export PAPYROS_TEMP_MNT=/mnt/tmp
+
 export HERACLES_PATH=/home/hselin/share/evimero/heracles/src
+
+export PAPYROS_TEST_FS_MOUNT_OPTIONS="-o kernelcache,0readbuffer"
+#export PAPYROS_TEST_FS_MOUNT_OPTIONS="-o 0readbuffer"
 
 
 export RECREATE_TEST_DEV=true
@@ -36,8 +40,9 @@ export RECREATE_TEST_DEV=true
 
 umount $TEST_DIR
 umount $SCRATCH_MNT
+umount $PAPYROS_TEMP_MNT
 
-mkdir -p $TEST_DIR $SCRATCH_MNT $MD_VOL$TEST_DEV $MD_VOL$SCRATCH_DEV
+mkdir -p $TEST_DIR $SCRATCH_MNT $MD_VOL$TEST_DEV $MD_VOL$SCRATCH_DEV $PAPYROS_TEMP_MNT
 
 
 source ./common/papyros
@@ -49,5 +54,52 @@ LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH _
 
 
 #LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check $*
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check -b -g quick
-LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check generic/034
+LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check -b -g quick
+#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check generic/451
+#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check generic/257
+
+
+
+#/usr/bin/timeout -s TERM 30s /home/hselin/share/evimero/xfstests/src/aio-dio-regress/aio-dio-cycle-write -c 999999 -b 655360 /home/hselin/share/evimero/heracles/mnt/foo
+
+
+
+
+#generic/124 \
+#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
+#generic/126 \
+#generic/128 \
+#generic/129
+
+
+
+
+
+
+# LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
+# generic/322 \
+# generic/324 \
+# generic/325 \
+# generic/326 \
+# generic/327 \
+# generic/328 \
+# generic/329 \
+# generic/330 \
+# generic/331 \
+# generic/332 \
+# generic/335 \
+# generic/336 \
+# generic/337 \
+# generic/338 \
+# generic/341 \
+# generic/342 \
+# generic/343 \
+# generic/346 \
+# generic/347 \
+# generic/348
+
+
+#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
+#generic/346 \
+#generic/347 \
+#generic/348
