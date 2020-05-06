1bab347028ef73e4848c8c92075a13f669d18333
hselin <hselin.chen@gmail.com>
2020-05-06 16:21:22 -0700

    Cleaned up check-papyros.sh

--- check-papyros.sh
+++ check-papyros.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+#export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 #set -x
 
 if [[ $EUID -ne 0 ]]; then
@@ -12,31 +11,33 @@ fi
 
 export PATH=$PATH:/usr/local/bin
 
-export MD_VOL=/md-vol
+export MD_VOL=/dev/shm/xfstests
 
 export FSTYP=papyros
-export TEST_DEV=/dev/loop0
-#export TEST_DEV=/dev/sda
-export TEST_SPARE_DEV=/dev/loop1
+
+export TEST_DEV=/dev/loop100
+export TEST_SPARE_DEV=/dev/loop101
 export TEST_DIR=/mnt/xfstests/test_dir
 export TEST_MD_DIR=$MD_VOL$TEST_DEV
 
-export SCRATCH_DEV=/dev/loop2
-#export SCRATCH_DEV=/dev/sdb
-export SCRATCH_SPARE_DEV=/dev/loop3
+#SCRATCH_DEV needs to be CMR so dm-thin can be created
+#May  5 06:25:22 corfu kernel: [13013.664214] device-mapper: table: 253:0: start=10240 not aligned to h/w zone size 524288 of sde
+#May  5 06:25:22 corfu kernel: [13013.664215] device-mapper: core: Cannot calculate initial queue limits
+#May  5 06:25:22 corfu kernel: [13013.664217] device-mapper: ioctl: unable to set up device queue for new table.
+export SCRATCH_DEV=/dev/loop102
+export SCRATCH_SPARE_DEV=/dev/loop103
 export SCRATCH_MNT=/mnt/xfstests/scratch_dir
 export SCRATCH_MD_DIR=$MD_VOL$SCRATCH_DEV
 
 export PAPYROS_TEMP_MNT=/mnt/tmp
 
-export HERACLES_PATH=/home/hselin/share/evimero/heracles/src
+export HERACLES_PATH=/home/hselin/share/heracles/src
 
 export PAPYROS_TEST_FS_MOUNT_OPTIONS="-o kernelcache,0readbuffer"
-#export PAPYROS_TEST_FS_MOUNT_OPTIONS="-o 0readbuffer"
-
 
 export RECREATE_TEST_DEV=true
 
+export LIB_FUSE_PATH=/home/hselin/share/libfuse/build/lib/
 
 umount $TEST_DIR
 umount $SCRATCH_MNT
@@ -44,62 +45,13 @@ umount $PAPYROS_TEMP_MNT
 
 mkdir -p $TEST_DIR $SCRATCH_MNT $MD_VOL$TEST_DEV $MD_VOL$SCRATCH_DEV $PAPYROS_TEMP_MNT
 
-
 source ./common/papyros
-LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH _mkfs_papyros $TEST_DEV $TEST_SPARE_DEV $TEST_MD_DIR
-LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH _mkfs_papyros $SCRATCH_DEV $SCRATCH_SPARE_DEV $SCRATCH_MD_DIR
 
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH _papyros_mount $SCRATCH_DEV $SCRATCH_MNT
-#exit
+LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH _mkfs_papyros $TEST_DEV $TEST_SPARE_DEV $TEST_MD_DIR
+LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH _mkfs_papyros $SCRATCH_DEV $SCRATCH_SPARE_DEV $SCRATCH_MD_DIR
 
-
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check $*
-LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check -b -g quick
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check generic/451
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check generic/257
-
-
-
-#/usr/bin/timeout -s TERM 30s /home/hselin/share/evimero/xfstests/src/aio-dio-regress/aio-dio-cycle-write -c 999999 -b 655360 /home/hselin/share/evimero/heracles/mnt/foo
-
-
-
-
-#generic/124 \
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
-#generic/126 \
-#generic/128 \
-#generic/129
-
-
-
-
-
-
-# LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
-# generic/322 \
-# generic/324 \
-# generic/325 \
-# generic/326 \
-# generic/327 \
-# generic/328 \
-# generic/329 \
-# generic/330 \
-# generic/331 \
-# generic/332 \
-# generic/335 \
-# generic/336 \
-# generic/337 \
-# generic/338 \
-# generic/341 \
-# generic/342 \
-# generic/343 \
-# generic/346 \
-# generic/347 \
-# generic/348
-
-
-#LD_LIBRARY_PATH=/home/hselin/share/evimero/libfuse/build/lib/:$LD_LIBRARY_PATH ./check \
-#generic/346 \
-#generic/347 \
-#generic/348
+if [ $# -eq 0 ]; then
+    LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH ./check -b -g quick
+else
+    LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH ./check "$@"
+fi
