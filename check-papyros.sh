1caa87234b8946267d30aa54aeec19371db3ac87
amyhyen <amyhyen@gmail.com>
2020-05-19 05:48:04 +0000

    updates to rc to make test 108 work. added env var overrides for BASE_PATH and *DEV

--- check-papyros.sh
+++ check-papyros.sh
@@ -15,8 +15,8 @@ export MD_VOL=/dev/shm/xfstests
 
 export FSTYP=papyros
 
-export TEST_DEV=/dev/loop100
-export TEST_SPARE_DEV=/dev/loop101
+if [[ -z ${TEST_DEV} ]]; then export TEST_DEV=/dev/loop100; fi
+if [[ -z ${TEST_SPARE_DEV} ]]; then export TEST_SPARE_DEV=/dev/loop101; fi
 export TEST_DIR=/mnt/xfstests/test_dir
 export TEST_MD_DIR=$MD_VOL$TEST_DEV
 
@@ -24,20 +24,23 @@ export TEST_MD_DIR=$MD_VOL$TEST_DEV
 #May  5 06:25:22 corfu kernel: [13013.664214] device-mapper: table: 253:0: start=10240 not aligned to h/w zone size 524288 of sde
 #May  5 06:25:22 corfu kernel: [13013.664215] device-mapper: core: Cannot calculate initial queue limits
 #May  5 06:25:22 corfu kernel: [13013.664217] device-mapper: ioctl: unable to set up device queue for new table.
-export SCRATCH_DEV=/dev/loop102
-export SCRATCH_SPARE_DEV=/dev/loop103
+if [[ -z ${SCRATCH_DEV} ]]; then export SCRATCH_DEV=/dev/loop102; fi
+if [[ -z ${SCRATCH_SPARE_DEV} ]]; then export SCRATCH_SPARE_DEV=/dev/loop103; fi
+
 export SCRATCH_MNT=/mnt/xfstests/scratch_dir
 export SCRATCH_MD_DIR=$MD_VOL$SCRATCH_DEV
 
 export PAPYROS_TEMP_MNT=/mnt/tmp
 
-export HERACLES_PATH=/home/hselin/share/heracles/src
+if [[ -z ${BASE_PATH} ]]; then export BASE_PATH=/home/hselin/share; fi
+
+export HERACLES_PATH=$BASE_PATH/heracles/src
 
 export PAPYROS_TEST_FS_MOUNT_OPTIONS="-o kernelcache,0readbuffer"
 
 export RECREATE_TEST_DEV=true
 
-export LIB_FUSE_PATH=/home/hselin/share/libfuse/build/lib/
+export LIB_FUSE_PATH=$BASE_PATH/libfuse/build/lib/
 
 umount $TEST_DIR
 umount $SCRATCH_MNT
@@ -50,8 +53,10 @@ source ./common/papyros
 LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH _mkfs_papyros $TEST_DEV $TEST_SPARE_DEV $TEST_MD_DIR
 LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH _mkfs_papyros $SCRATCH_DEV $SCRATCH_SPARE_DEV $SCRATCH_MD_DIR
 
+
 if [ $# -eq 0 ]; then
     LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH ./check -b -g quick
 else
     LD_LIBRARY_PATH=$LIB_FUSE_PATH:$LD_LIBRARY_PATH ./check "$@"
 fi
+
